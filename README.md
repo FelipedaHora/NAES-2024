5db938043f60cd4939cdc1e8ba4447a70e7e4d1c
= <=>
2024-10-21 20:54:20 -0300

    NEW - implementacoes na tela principal

--- README.md
+++
@@ -1,5 +0,0 @@
-diagrama de caso de uso, representar com os atores e não utilizar as flechas
-
-sem separação entre os atributos, devem ser tipados
-
-fazer a ligação com projeto com fatura
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from cadastros.models import Projeto, Tarefa
 
 # Criar uma view para a página inicial
 # com herança para a classe TemplateView
@@ -8,6 +9,14 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = "Página Inicial"
+        context['qtde_projetos'] = Projeto.objects.all().count()
+        
+        if(self.request.user.is_authenticated):
+            context['m_tarefas'] = Tarefa.objects.count()
+            
+        context['tarefas'] = Tarefa.objects.all().order_by('nome')
+        #context['projeto'] = Projeto.objects.get(pk=1)
+                    
         return context
     
     
